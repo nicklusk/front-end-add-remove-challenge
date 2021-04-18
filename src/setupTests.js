e50fe0b951eae90b23cb91fe5bd649712d895383
Nick Lusk <r.nick.lusk@gmail.com>
2021-04-18 01:51:51 -0400

    scan

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom/extend-expect';
-import Enzyme, configure from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-Enzyme.configure({ adapter: new Adapter() });
+
+// import Enzyme, configure from 'enzyme';
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// Enzyme.configure({ adapter: new Adapter() });
